4d880a1b83a1ffe84a8f2304c0578f504493f990
BhashithaArambepola <barambepola@gmail.com>
2022-12-05 10:20:55 +0530

    add component to services page

---
+++ src/services/our-services.js
@@ -0,0 +1,189 @@
+import { LitElement, css, html } from "lit";
+import serviceicon1 from "./../assets/icons/bulb-yellow.svg";
+import serviceicon2 from "./../assets/icons/bulb-yellow.svg";
+
+export class OurServices extends LitElement {
+  render() {
+    return html`
+      <section class="our-services">
+        <div class="our-services-area-box">
+          <h3 class="our-services__sub-header">Our services</h3>
+          <h1 class="our-services__header">How Can We Assist You</h1>
+          <div class="our-services__card-area">
+            <div class="our-services__card">
+              <div class="our-services__image-complex">
+                <img
+                  src=${serviceicon1}
+                  alt="bulb image"
+                  class="our-services__card-image"
+                />
+              </div>
+              <h3 class="our-services__card-header">
+                Online Charging System (OCS)
+              </h3>
+              <p class="our-services__card-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam iste quas eius quibusdam, culpa autem nihil laudantium
+                delectus consequatur non provident animi beatae excepturi vitae
+                aliquid cumque.
+              </p>
+              <p class="our-services__card-description">
+                Ipsam, et temporibus dolorum deleniti at quasi officia tenetur
+                non provident eum repudiandae obcaecati sunt sequi doloremque
+                modi quod voluptatum mollitia autem praesentium voluptatem minus
+                nostrum vel! Beatae atque similique repellat vel aliquid fuga
+                labore totam magnam qui, perspiciatis aperiam optio possimus
+                voluptatibus obcaecati saepe, cumque at voluptatum corrupti enim
+                voluptas. Suscipit, pariatur autem? Autem vero quae aut facilis,
+                iste necessitatibus perspiciatis commodi qui tempora nesciunt
+                doloremque, sint aliquam! Aliquid nostrum perferendis ipsa
+                ducimus blanditiis
+              </p>
+            </div>
+
+            <div class="our-services__card">
+              <div class="our-services__image-complex">
+                <img
+                  src=${serviceicon2}
+                  alt="bulb image"
+                  class="our-services__card-image"
+                />
+              </div>
+              <h3 class="our-services__card-header">
+                Online Charging System (OCS)
+              </h3>
+              <p class="our-services__card-description">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam iste quas eius quibusdam, culpa autem nihil laudantium
+                delectus consequatur non provident animi beatae excepturi vitae
+                aliquid cumque.
+              </p>
+              <p class="our-services__card-description">
+                Ipsam, et temporibus dolorum deleniti at quasi officia tenetur
+                non provident eum repudiandae obcaecati sunt sequi doloremque
+                modi quod voluptatum mollitia autem praesentium voluptatem minus
+                nostrum vel! Beatae atque similique repellat vel aliquid fuga
+                labore totam magnam qui, perspiciatis aperiam optio possimus
+                voluptatibus obcaecati saepe, cumque at voluptatum corrupti enim
+                voluptas. Suscipit, pariatur autem? Autem vero quae aut facilis,
+                iste necessitatibus perspiciatis commodi qui tempora nesciunt
+                doloremque, sint aliquam! Aliquid nostrum perferendis ipsa
+                ducimus blanditiis
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    `;
+  }
+
+  static styles = css`
+    .our-services {
+      min-height: 100vh;
+      display: flex;
+      padding: 80px 0;
+      font-family: "sf-pro-text", sans-serif;
+      scroll-snap-align: start;
+      /* background-color: green; */
+    }
+
+    .our-services__sub-header {
+      color: var(--color-primary);
+      font-size: 24px;
+      font-family: "sf-pro-display-semi", sans-serif;
+      text-transform: uppercase;
+      letter-spacing: 0.15em;
+      margin-bottom: 0px;
+    }
+
+    .our-services__header {
+      font-size: 36px;
+      letter-spacing: 0.05em;
+      font-family: "sf-pro-display-semi", sans-serif;
+      margin-bottom: 30px;
+      margin-top: 10px;
+    }
+
+    .our-services__card {
+      /* width: 100%; */
+      border-bottom: 4px var(--color-primary) solid;
+      padding: 50px 40px;
+      box-shadow: 0px 4px 20px 2px rgba(0, 0, 0, 0.1);
+      border-radius: 10px;
+      margin-bottom: 30px;
+      transition: 0.3s all;
+    }
+
+    .our-services__image-complex {
+      position: relative;
+      margin-bottom: 110px;
+    }
+
+    .our-services__card-image {
+      position: absolute;
+      width: 50px;
+      margin-bottom: 30px;
+      transition: 0.3s all;
+    }
+
+    .our-services__card-header {
+      font-size: 20px;
+      margin-bottom: 15px;
+    }
+
+    .our-services__card-description {
+      color: var(--color-description);
+      font-size: 16px;
+      margin-bottom: 25px;
+    }
+
+    .our-services__card-link {
+      text-decoration: none;
+      font-size: 14px;
+      letter-spacing: 0.05em;
+      color: var(--color-primary);
+      transition: 0.3s all;
+    }
+
+    /* ---------------------------------------------------------- */
+
+    .our-services-area-box {
+      margin: auto auto;
+      display: flex;
+      flex-direction: column;
+      max-width: 1400px;
+      height: 100%;
+    }
+
+    @media (min-width: 1200px) {
+      .our-services-area-box {
+        max-width: 1140px !important;
+        padding-left: 5rem !important;
+        padding-right: 5rem !important;
+      }
+    }
+
+    @media (min-width: 1400px) {
+      .our-services-area-box {
+        max-width: 1400px !important;
+      }
+    }
+
+    @media (max-width: 1200px) {
+      .our-services-area-box {
+        padding-left: 5rem !important;
+        max-width: 1140px !important;
+        padding-right: 5rem !important;
+      }
+    }
+
+    @media (max-width: 576px) {
+      .our-services-area-box {
+        padding-left: 2rem !important;
+        padding-right: 2rem !important;
+      }
+    }
+  `;
+}
+
+window.customElements.define("our-services", OurServices);
--- src/services/services.js
+++ src/services/services.js
@@ -1,141 +1,144 @@
 import { LitElement, css, html } from "lit";
+import "./our-services";
 import landingImage from "./../assets/images/about-us-landing.png";
 
 export class Services extends LitElement {
-    static styles = css`
-        .landing-services-banner {
-            display: flex;
-            align-items: flex-end;
-            font-family: "sf-pro-text", sans-serif;
-            scroll-snap-align: start;
-            height: calc(40vh - 90px);
-            width: 100vw;
-            overflow: hidden;
-            position: relative;
-        }
-
-        .background-image-service-landing {
-            object-fit: cover;
-            position: absolute;
-            /* display: none; */
-            width: 100%;
-            height: 100%;
-            top: 0;
-            left: 0;
-        }
-
-        .overlay {
-            background: rgba(0, 0, 0, 0.5);
-            height: 100%;
-            width: 100%;
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-        .landing-services-banner > .content-area-box {
-            width: 1400px;
-            margin: auto;
-            margin-bottom: 4rem;
-        }
-
-        .landing-banner__content {
-            position: absolute;
-            display: block;
-            z-index: 300;
-            /* top: 0%; */
-            transform: translateY(-50%);
-            margin-bottom: 90px;
-        }
-
-        .landing-banner__links {
-            display: flex;
-            align-items: flex-start;
-            margin-bottom: 80px;
-        }
-
-        .landing-banner__header {
-            font-family: "sf-pro-display-semi";
-            font-size: 36px;
-            color: var(--color-white);
-            margin-bottom: 1rem;
-        }
-
-        .landing-banner__nav {
-            font-size: 20px;
-            color: #1df2ba;
-            text-decoration: none;
-            height: 30px;
-        }
-
-        .landing-banner__divider {
-            display: inline-block;
-            margin: 0 10px;
-            width: 3px;
-            height: 25px;
-            background-color: var(--color-primary);
-            color: var(--color-primary);
-        }
-        /* --------------------------------------------------------- */
-
-        .our-services-area-box {
-            margin: auto auto;
-            display: flex;
-            flex-direction: column;
-            max-width: 1400px;
-            height: 100%;
-        }
-
-        @media (min-width: 1200px) {
-            .our-services-area-box {
-                max-width: 1140px !important;
-                padding-left: 5rem !important;
-                padding-right: 5rem !important;
-            }
-        }
-
-        @media (min-width: 1400px) {
-            .our-services-area-box {
-                max-width: 1400px !important;
-            }
-        }
-
-        @media (max-width: 1200px) {
-            .our-services-area-box {
-                padding-left: 5rem !important;
-                max-width: 1140px !important;
-                padding-right: 5rem !important;
-            }
-        }
-
-        @media (max-width: 576px) {
-            .our-services-area-box {
-                padding-left: 2rem !important;
-                padding-right: 2rem !important;
-            }
-        }
-    `;
-    render() {
-        return html`
-            <section class="landing-services-banner">
-                <div class="content-area-box">
-                    <div class="landing-banner__content">
-                        <h1 class="landing-banner__header">Services</h1>
-                        <div class="landing-banner__links">
-                            <a class="landing-banner__nav" href="#">Home</a
-                            ><span class="landing-banner__divider"></span
-                            ><a href="#" class="landing-banner__nav">Services</a>
-                        </div>
-                    </div>
-                </div>
-                <img
-                    src=${landingImage}
-                    alt="background image"
-                    class="background-image-service-landing"
-                />
-                <div class="overlay"></div>
-            </section>
-        `;
+  static styles = css`
+    .landing-services-banner {
+      display: flex;
+      align-items: flex-end;
+      font-family: "sf-pro-text", sans-serif;
+      scroll-snap-align: start;
+      height: calc(40vh - 90px);
+      width: 100vw;
+      overflow: hidden;
+      position: relative;
     }
+
+    .background-image-service-landing {
+      object-fit: cover;
+      position: absolute;
+      /* display: none; */
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+    }
+
+    .overlay {
+      background: rgba(0, 0, 0, 0.5);
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    .landing-services-banner > .content-area-box {
+      width: 1400px;
+      margin: auto;
+      margin-bottom: 4rem;
+    }
+
+    .landing-banner__content {
+      position: absolute;
+      display: block;
+      z-index: 300;
+      /* top: 0%; */
+      transform: translateY(-50%);
+      margin-bottom: 90px;
+    }
+
+    .landing-banner__links {
+      display: flex;
+      align-items: flex-start;
+      margin-bottom: 80px;
+    }
+
+    .landing-banner__header {
+      font-family: "sf-pro-display-semi";
+      font-size: 36px;
+      color: var(--color-white);
+      margin-bottom: 1rem;
+    }
+
+    .landing-banner__nav {
+      font-size: 20px;
+      color: #1df2ba;
+      text-decoration: none;
+      height: 30px;
+    }
+
+    .landing-banner__divider {
+      display: inline-block;
+      margin: 0 10px;
+      width: 3px;
+      height: 25px;
+      background-color: var(--color-primary);
+      color: var(--color-primary);
+    }
+    /* --------------------------------------------------------- */
+
+    /* .our-services-area-box {
+      margin: auto auto;
+      display: flex;
+      flex-direction: column;
+      max-width: 1400px;
+      height: 100%;
+    } */
+
+    @media (min-width: 1200px) {
+      .content-area-box {
+        max-width: 1140px !important;
+        padding-left: 5rem !important;
+        padding-right: 5rem !important;
+      }
+    }
+
+    @media (min-width: 1400px) {
+      .content-area-box {
+        max-width: 1400px !important;
+      }
+    }
+
+    @media (max-width: 1200px) {
+      .content-area-box {
+        padding-left: 5rem !important;
+        max-width: 1140px !important;
+        padding-right: 5rem !important;
+      }
+    }
+
+    @media (max-width: 576px) {
+      .content-area-box {
+        padding-left: 2rem !important;
+        padding-right: 2rem !important;
+      }
+    }
+  `;
+  render() {
+    return html`
+      <section class="landing-services-banner">
+        <div class="content-area-box">
+          <div class="landing-banner__content">
+            <h1 class="landing-banner__header">Services</h1>
+            <div class="landing-banner__links">
+              <a class="landing-banner__nav" href="#">Home</a
+              ><span class="landing-banner__divider"></span
+              ><a href="#" class="landing-banner__nav">Services</a>
+            </div>
+          </div>
+        </div>
+        <img
+          src=${landingImage}
+          alt="background image"
+          class="background-image-service-landing"
+        />
+        <div class="overlay"></div>
+      </section>
+      <our-services></our-services>
+      <app-footer></app-footer>
+    `;
+  }
 }
 
 window.customElements.define("app-services", Services);
