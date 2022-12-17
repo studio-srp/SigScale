b8b364850b85335ffd40e67565c70bf049fad1e3
Mshihan <167sihan@gmail.com>
2022-12-17 23:29:20 +0530

    favicon added

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,28 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+// export default defineConfig({
+//     build: {
+//         lib: {
+//             entry: "src/my-app.js",
+//             formats: ["es"],
+//         },
+//         rollupOptions: {
+//             external: /^lit/,
+//         },
+//     },
+// });
+
+export default defineConfig(({ command, mode }) => {
+  return {
     build: {
-        lib: {
-            entry: "src/my-app.js",
-            formats: ["es"],
-        },
-        rollupOptions: {
-            external: /^lit/,
-        },
+      lib: {
+        entry: "src/my-app.js",
+        formats: ["es"],
+      },
+      rollupOptions: {
+        external: mode === "production" ? "" : /^lit/,
+      },
     },
+  };
 });
